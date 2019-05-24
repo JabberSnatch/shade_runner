543ca85f4dde3e6fceca2da96543fd0c7b5fdf96
Samuel Bourasseau <samuel.bourasseau@allegorithmic.com>
2019-05-24 09:43:45 +0200

    Cleaned ray generation from matrix

--- kernels/raymarching_exp.frag.glsl
+++ kernels/raymarching_exp.frag.glsl
@@ -7,8 +7,8 @@ mat4 perspective_inverse_hfov(float n, float f, float alpha, float aspect)
 	return mat4(
 		tan_half_alpha, 0.0, 0.0, 0.0,
 		0.0, aspect * tan_half_alpha, 0.0, 0.0,
-		0.0, 0.0, 0.0, -1.0,
-		0.0, 0.0, (f-n)/(-2.0*f*n), (f+n)/(2.0*f*n)
+		0.0, 0.0, 0.0, (f-n)/(-2.0*f*n),
+		0.0, 0.0, 1.0, (f+n)/(2.0*f*n)
 	);
 }
 
@@ -33,12 +33,9 @@ vec3 target_half_diagonal_hfov(float n, float alpha, float aspect)
 vec3 compute_ray_matrix(mat4 perspective_inverse, vec2 clip_coord)
 {
 	vec4 target = vec4(clip_coord, 1.0, 1.0);
-	vec4 origin = vec4(clip_coord, -1.0, 1.0);
 	vec4 ray_direction = perspective_inverse * target;
 	ray_direction = ray_direction / ray_direction.w;
-	vec4 ray_origin = perspective_inverse * origin;
-	ray_origin = ray_origin / ray_origin.w;
-	return normalize((ray_direction - ray_origin).xyz);
+	return normalize(ray_direction.xyz);
 }
 
 vec3 compute_ray_plane(vec3 half_diagonal, vec2 clip_coord)
@@ -51,7 +48,7 @@ void imageMain(inout vec4 frag_color, vec2 frag_coord)
 {
 	float aspect_ratio = iResolution.y / iResolution.x;
 	float fov = 90.0 * pi / 180.0;
-	float near = 1000.0;
+	float near = 0.001;
 	float far = 10000.0;
 	vec2 clip_coord = ((frag_coord / iResolution) - 0.5) * 2.0;
 
@@ -64,19 +61,18 @@ void imageMain(inout vec4 frag_color, vec2 frag_coord)
 #endif
 
 
-#if 0
+#if 1
 // ================================================================================
 // MATRIX
 // ================================================================================
 
 	frag_color.xyz = abs(compute_ray_matrix(perspective_inverse, clip_coord));
 
-#if 0
-	vec3 target = vec3(clip_coord, 1.0);
-	vec3 origin = vec3(clip_coord, -1.0);
-	vec3 ray_direction = mat3(perspective_inverse) * target;
-	vec3 ray_origin = mat3(perspective_inverse) * origin;
-	frag_color.xyz = abs(normalize(ray_direction - ray_origin));
+#if 1
+	vec4 target = vec4(clip_coord, 1.0, 1.0);
+	vec4 ray_direction = perspective_inverse * target;
+    ray_direction = ray_direction / ray_direction.w;
+	frag_color.xyz = (normalize(ray_direction.xyz));
 #endif
 
 	vec3 side1 = compute_ray_matrix(perspective_inverse, vec2(-1.0, 0.0));
@@ -87,13 +83,13 @@ void imageMain(inout vec4 frag_color, vec2 frag_coord)
 // PLANE (baseline)
 // ================================================================================
 
-	frag_color.xyz = abs(compute_ray_plane(half_diagonal, clip_coord));
+	frag_color.xyz = (compute_ray_plane(half_diagonal, clip_coord));
 
 	vec3 side1 = compute_ray_plane(half_diagonal, vec2(-1.0, 0.0));
 	vec3 side2 = compute_ray_plane(half_diagonal, vec2(1.0, 0.0));
 #endif
 
-#if 1
+#if 0
 	vec3 matrix_ray = compute_ray_matrix(perspective_inverse, clip_coord);
 	vec3 plane_ray = compute_ray_plane(half_diagonal, clip_coord);
 	frag_color.xyz = abs(plane_ray - matrix_ray);
